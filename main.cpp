29535ace31284df98ade8c13e247ec035fa0b4a2
aidanachtziger <aidan.achtziger@gmail.com>
2025-07-20 21:28:37 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // Define variables
+    std::string firstName = "Aidan";
+    std::string lastName = "Achtziger";
+    std::string streetAddress = "123 Main Street";
+    std::string city = "Erie";
+    std::string zipCode = "90210";
+
+    // Display info
+    std::cout << "First Name: " << firstName << std::endl;
+    std::cout << "Last Name: " << lastName << std::endl;
+    std::cout << "Street Address: " << streetAddress << std::endl;
+    std::cout << "City: " << city << std::endl;
+    std::cout << "Zip Code: " << zipCode << std::endl;
+
+    return 0;
+}
