028c72aa9ec733eec88da3344699755fe45903a8
aidanachtziger <aidan.achtziger@gmail.com>
2025-07-20 21:37:42 -0400

    Add files via upload

---
+++ mod1-1_corrected.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//#include<conio.h>
+//Standard namespace declaration
+//using namespace std;
+//Main Function
+//int main()
+//{
+//Standard Ouput Statement
+//cout<<"Welcome to this C++ Program"<<endl;
+//cout<<I have corrected all errors for this program. <<endl;
+// Wait For Output Screen
+//Main Function return Statement
+//}
+
+// corrected code:
+
+#include <iostream>   // Correct header for I/O
+using namespace std;  // Standard namespace declaration
+
+int main() {  // Main function must return int
+    // Standard Output Statements
+    cout << "Welcome to this C++ Program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+
+    // Wait for user input before closing (optional)
+    cin.get();  // Only works if you include <iostream>
+
+    return 0;   // Main function return statement
+}
---
+++ mod1-2_corrected.cpp
@@ -0,0 +1,37 @@
+//Standard namespace declaration
+//using namespace std;
+//Main Function
+//int main()
+//{
+//double myMoney = 1000.50; //this should be printed out
+//Standard Ouput Statement
+//cout"Please be sure to correct all syntax errors in this program"<<endl;
+//cout<<"I have corrected all errors for this program. <<endl;
+//cout<<" The total amount of money available is = "<< <<endl;
+// Wait For Output Screen
+//Main Function return Statement
+//return 0;
+//}
+
+// corrected code:
+
+#include <iostream>      // Include for input/output
+using namespace std;     // Standard namespace declaration
+
+int main() {             // Main function
+
+    double myMoney = 1000.50; // This should be printed out
+
+    // Standard Output Statements
+    cout << "Please be sure to correct all syntax errors in this program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+    cout << "The total amount of money available is = " << myMoney << endl;
+
+    // Wait for user input (optional)
+    cin.get();
+
+    return 0;            // Main function return statement
+}
+
+
+
